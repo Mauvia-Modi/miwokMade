937f7ec031d0fe62620922d6c2f5016e39729e0e
Shahid Modi <mauviamodi@gmail.com>
2018-01-19 19:39:53 +0530

    Java files added.

---
+++ CategoryPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Mauvia Modi on 21-12-2017.
+ */
+
+public class CategoryPagerAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public CategoryPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return new NumbersFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
+            default:
+                return "error";
+        }
+    }
+}
